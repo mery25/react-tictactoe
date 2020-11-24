451ce5c9200347b333d95e19e573db1ac2cc7342
Maria Tibau <maria.ta.89@gmail.com>
2020-11-24 21:39:45 +0100

    Save square values on the state of board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,16 @@ import React from "react"
 import Square from "./Square"
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]} />;
     }
   
     render() {
