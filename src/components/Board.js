a4bfa456b03dfcb59fe1e68cb9bb994a83b01a1d
Maria Tibau <maria.ta.89@gmail.com>
2020-11-25 18:08:58 +0100

    Refactor square rendering

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,46 @@
 import React from "react"
 import Square from "./Square"
 
+const NUM_ROWS = 3;
+const NUM_COLS = 3;
+
 class Board extends React.Component {
- 
+
     renderSquare(i) {
         return <Square
+            key={i}
             value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
         />;
     }
+    
+    renderRow(fromPosition, toPosition) {
+        let boardSquares = [];
+        for (let j = fromPosition; j < toPosition; ++j) {
+            boardSquares.push(this.renderSquare(j));
+        }
+        return boardSquares;
+    }
+
+    renderRows() {
+        let boardRows = [];
+        for (let i = 0; i < NUM_ROWS * NUM_COLS; i += NUM_ROWS) {
+            let boardRow = this.renderRow(i, i + NUM_COLS);
+            boardRows.push(
+                (<div className="board-row">
+                    {boardRow}
+                </div>)
+            );
+        }
+        return boardRows;
+    }
 
     render() {
+       let boardRows = this.renderRows();
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {boardRows}
             </div>
         );
     }
