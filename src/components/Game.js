49980eaf5849c3bc49ffb27317a3d4353d51f23b
Maria Tibau <maria.ta.89@gmail.com>
2020-11-25 10:35:02 +0100

    Move logic to set the status from board to game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,59 @@
 import '../css/Game.css';
 import React from "react";
 import Board from "./Board";
+import { calculateWinner } from "../calculateWinner"
 
 class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+    constructor() {
+        super()
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
     }
-  }
-  
-  export default Game;
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState(prevState => ({
+            squares: squares,
+            xIsNext: !prevState.xIsNext
+        }));
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1]
+        const winner = calculateWinner(current.squares)
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
