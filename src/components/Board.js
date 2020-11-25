3aaadc924030810d75fb03e6557734fdadde2140
Maria Tibau <maria.ta.89@gmail.com>
2020-11-25 10:00:38 +0100

    Add logic to paint on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,15 +6,19 @@ class Board extends React.Component {
         super(props);
 
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState(prevState => ({ 
+            squares: squares,
+            xIsNext: !prevState.xIsNext
+        }));
     }
 
     renderSquare(i) {
