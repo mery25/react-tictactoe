30a3c26c080615d5f5d306b0d3e8b716fdc660a4
Maria Tibau <maria.ta.89@gmail.com>
2020-11-26 09:43:54 +0100

    Change the SwitchButton to use className instead of style

--- src/components/SwitchOrderButton.js
+++ src/components/SwitchOrderButton.js
@@ -1,23 +1,20 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons'
-
-const SELECTED_COLOR_STYLE = { color: "black" }
-const UNSELECTED_COLOR_STYLE = { color: "grey" }
+import '../css/SwitchButton.css';
 
 function SwitchOrderButton(props) {
 
-    let downArrowStyle = SELECTED_COLOR_STYLE;
-    let upArrowStyle = UNSELECTED_COLOR_STYLE;
-
-    if (!props.isAscending) {
-        downArrowStyle = UNSELECTED_COLOR_STYLE;
-        upArrowStyle = SELECTED_COLOR_STYLE;
-    }
     return (
         <button onClick={() => props.toggleOrder()}>
-            <FontAwesomeIcon style={downArrowStyle} icon={faArrowDown} />
-            <FontAwesomeIcon style={upArrowStyle} icon={faArrowUp} />
+            <FontAwesomeIcon 
+                className={props.isAscending ? "selected": "unselected"} 
+                icon={faArrowDown} 
+            />
+            <FontAwesomeIcon 
+                className={props.isAscending ? "unselected": "selected"} 
+                icon={faArrowUp} 
+            />
         </button>
     );
 }
