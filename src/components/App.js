24d7477b6bd816aa63f5b2142ccd0489dad22c3a
Maria Tibau <maria.ta.89@gmail.com>
2020-12-23 14:28:39 +0100

    Created an App component and css

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import GameContainer from "./GameContainer"
+import Header from "./Header"
+import "../css/App.css"
+
+function App() {
+
+    return (
+        <div>
+            <Header />
+            <GameContainer />
+        </div>
+    );
+}
+
+export default App;
