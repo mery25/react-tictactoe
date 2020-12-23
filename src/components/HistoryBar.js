c7293b050dc55016e57f28e87df052e6c4116a99
Maria Tibau <maria.ta.89@gmail.com>
2020-12-23 09:15:12 +0100

    Create separated history bar component and css

---
+++ src/components/HistoryBar.js
@@ -0,0 +1,49 @@
+import React from "react"
+import '../css/HistoryBar.css';
+import SwitchOrderButton from "./SwitchOrderButton"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleUp, faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
+
+function HistoryBar(props) {
+    var sidebarClass = props.isOpen ? 'history-bar open' : 'history-bar';
+    return (
+        <div className={sidebarClass}>
+            <label className="history-bar-toggle">
+                <FontAwesomeIcon 
+                    className={"toggle-icon"} 
+                    icon={ props.isOpen ? faAngleDoubleUp : faAngleDoubleDown} 
+                />
+                <span>H</span>
+                <span>I</span>
+                <span>S</span>
+                <span>T</span>
+                <span>O</span>
+                <span>R</span>
+                <span>Y</span>
+                <FontAwesomeIcon 
+                    className={"toggle-icon"}
+                    icon={ props.isOpen ? faAngleDoubleUp : faAngleDoubleDown}
+                />
+                <input
+                    id="collapsible"
+                    className="toggle"
+                    name="isChecked"
+                    type="checkbox"
+                    checked={props.isOpen}
+                    onChange={props.toggleSidebar} />
+            </label>
+            <div className="history-bar-content">
+                <div className="history-container">  
+                    <SwitchOrderButton 
+                                isAscending={props.isAscendingHistory} 
+                                toggleOrder={props.toggleOrder} 
+                            />  
+                    <ol className="history">{props.sortedMoves}</ol>
+                </div>
+            </div>
+           {/*  <button onClick={props.toggleSidebar} className="sidebar-toggle">HISTORY</button> */}
+        </div>
+      );
+};
+
+export default HistoryBar
