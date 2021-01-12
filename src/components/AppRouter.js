3b2d0bc58dacebc0526d8fdb2aec717102943735
Maria Tibau <maria.ta.89@gmail.com>
2021-01-12 12:24:48 +0100

    Add home route to the app router

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import GameContainer from "./game/GameContainer"
+import Home from "./home/Home"
 
 const AppRouter = () => {
     return (
         <Switch>
+            <Route exact path="/" component={Home} />
             <Route path="/game" component={GameContainer} />
         </Switch>
     );
